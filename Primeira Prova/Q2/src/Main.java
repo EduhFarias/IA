9b6f69d21d3681ae12bfc4b82560cc6a7539314e
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-07-17 10:51:47 -0300

    Update Main.java

--- Primeira Prova/Q2/src/Main.java
+++ Primeira Prova/Q2/src/Main.java
@@ -1,5 +1,20 @@
-public class Main{
-	public static void main(String[] args){
-	
-	}
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int distance[][] = new int[14][14];
+
+        for(int i = 0; i < 14; i++){
+            for(int j = 0; j < 14; j++){
+                distance[i][j] = input.nextInt();
+            }
+        }
+
+        String test = "E1";
+        if(test.contains("2")){
+            System.out.println("Ok");
+        }
+    }
 }
