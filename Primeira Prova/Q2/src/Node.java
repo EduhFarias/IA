294c51abaf6dc7a469deeca93db9c9a9b03198b0
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-07-19 15:12:33 -0300

    Update Node.java

--- Primeira Prova/Q2/src/Node.java
+++ Primeira Prova/Q2/src/Node.java
@@ -1,89 +1,67 @@
 import java.util.*;
 
-public class Node {
-    private int station;
-    private String line;
-    private Node father;
-    private List<Node> children;
-    private int heuristic;
-    private int depth;
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
 
-    public Node(int station, String line, Node father, List<Node> children, int heuristic, int depth) {
-        this.station = station;
-        this.line = line;
-        this.father = father;
-        this.children = new ArrayList<>();
-        this.heuristic = heuristic;
-        this.depth = depth;
+        int distance[][] = new int[14][14];
+        Util.generateMatrix(distance);
+        ArrayList<Node> nodes = new ArrayList<>();
+        Util.generateNode(nodes);
+        ArrayList<Node> path = new ArrayList<>();
+
+        System.out.println("Estação de embarque: (Somente o numero)");
+        int in = input.nextInt() - 1;
+        System.out.println("Estação de desembarque: (Somente o numero)");
+        int out = input.nextInt() - 1;
+
+        Node root = null;
+        Node goal = null;
+        try {
+            root = Node.getNode(in, nodes);
+            goal = Node.getNode(out, nodes);
+        } catch (NullPointerException e){
+            System.out.println("Invalido");
+            System.exit(1);
+        }
+        search(root, goal, path, 0);
+        for(Node current : path){
+            System.out.println("E" + current.getStation());
+        }
     }
 
-    public int getStation() {
-        return station;
+    public static void search(Node station, Node goal, ArrayList<Node> path, double depth){
+        if(station == goal){
+            path.add(station);
+            return;
+        } else{
+            double deeper = 1000000;
+            Node choice = null;
+            for(Node current : station.getChildren()){
+                if(depth < deeper){
+                    deeper = depth;
+                    choice = current;
+                }
+            }
+            path.add(choice);
+            search(choice, goal, path, depth);
+        }
     }
 
-    public void setStation(int station) {
-        this.station = station;
+    public static int heuristic(int distance[][], Node station, Node goal){
+        int h = distance[station.getStation()][goal.getStation()];
+        return h;
     }
 
-    public String getLine() {
-        return line;
-    }
-
-    public void setLine(String line) {
-        this.line = line;
-    }
-
-    public Node getFather() {
-        return father;
-    }
-
-    public void setFather(Node father) {
-        this.father = father;
-    }
-
-    public List<Node> getChildren() {
-        return children;
-    }
-
-    public void setChildren(List<Node> children) {
-        this.children = children;
-    }
-
-    public int getHeuristic() {
-        return heuristic;
-    }
-
-    public void setHeuristic(int heuristic) {
-        this.heuristic = heuristic;
-    }
-
-    public int getDepth() {
+    public static int depth(int distance[][], Node root, Node station){
+        int depth = 0;
         return depth;
     }
 
-    public void setDepth(int depth) {
-        this.depth = depth;
+    public static double evaluation(Node current, Node goal, int distance[][], int depth){
+        int total = heuristic(distance, current, goal) + depth;
+        return total/0.5;
     }
 
-    public static void cDepth(Node node){
-        Node aux;
-        int cont = 0;
-        boolean condition = true;
-        while(condition){
-            aux = node.getFather();
-            //cont += //matriz[aux.getStation()][node.getStation()];
-            //Acessa a tabela de distancia entre pontos para pegar a diff entre o no atual e seu pai
-            if(aux == null) condition = false;
-        }
-    }
-
-    public static Node getRoot(int in, ArrayList<Node> nodes){
-        for(Node current : nodes){
-            if(current.getStation() == in){
-                return current;
-            }
-        }
-        return null;
-    }
 
 }
