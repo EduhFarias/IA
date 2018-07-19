4a1ecb61ad30279f0ab83ad56b3e9d6741f3ab8f
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-07-19 10:43:15 -0300

    Update Main.java

--- Primeira Prova/Q2/src/Main.java
+++ Primeira Prova/Q2/src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         Util.generateMatrix(distance);
         ArrayList<Node> nodes = new ArrayList<>();
         Util.generateNode(nodes);
+        ArrayList<Node> path = new ArrayList<>();
 
         System.out.println("Estação de embarque: (Somente o numero)");
         int in = input.nextInt() - 1;
@@ -15,24 +16,38 @@ public class Main {
         int out = input.nextInt() - 1;
 
         Node root = null;
+        Node goal = null;
         try {
-            root = Node.getRoot(in, nodes);
+            root = Node.getNode(in, nodes);
+            goal = Node.getNode(out, nodes);
         } catch (NullPointerException e){
             System.out.println("Invalido");
             System.exit(1);
         }
-        
-        search(root, out);
+        //search(root, goal, path, 0);
     }
 
-    public static void search(Node root, int out){
-
+    public static void search(Node station, Node goal, ArrayList<Node> path, double depth){
+        if(station == goal){
+            return;
+        } else if(){
+            return;
+        }
     }
 
-    public static int evaluation( ){
-        int result = 1;
+    public static int heuristic(int distance[][], Node station, Node goal){
+        int h = distance[station.getStation()][goal.getStation()];
+        return h;
+    }
 
-        return result;
+    public static int depth(int distance[][], Node root, Node station){
+        int depth = 0;
+        return depth;
+    }
+
+    public static double evaluation(Node current, Node goal, int distance[][], int depth){
+        int total = heuristic(distance, current, goal) + depth;
+        return total/0.5;
     }
 
 
