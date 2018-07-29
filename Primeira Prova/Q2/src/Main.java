71c6f6cbe647e91ba81d638df43267d83f45b17c
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-07-29 18:57:14 -0300

    Update Main.java

--- Primeira Prova/Q2/src/Main.java
+++ Primeira Prova/Q2/src/Main.java
@@ -9,9 +9,9 @@ public class Main {
         ArrayList<Node> path = new ArrayList<>();
 
         System.out.println("Estação de embarque: (Somente o numero)");
-        int in = input.nextInt() - 1;
+        int in = input.nextInt();
         System.out.println("Estação de desembarque: (Somente o numero)");
-        int out = input.nextInt() - 1;
+        int out = input.nextInt();
 
         Node root = null;
         Node goal = null;
@@ -22,7 +22,10 @@ public class Main {
             System.out.println("Invalido");
             System.exit(1);
         }
+
+        path.add(root);
         search(root, goal, path, 0);
+
         for(Node current : path){
             System.out.println("E" + current.getStation());
         }
@@ -30,23 +33,30 @@ public class Main {
 
     public static void search(Node station, Node goal, ArrayList<Node> path, double depth){
         if(station == goal){
-            path.add(station);
             return;
         } else{
             double deeper = 1000000;
             Node choice = null;
-            for(Node current : station.getChildren()){
-                if(Util.generateMatrix(current.getStation(), station.getStation()) < deeper){
-                    if(station.getFather() != null || !station.getFather().getLine().contains(current.getLine())){
-                        
+            int h = 0;
+            double total = 0;
+
+            for(Node current : station.getChildren()){ //loop para checar para qual filho vai
+                h = Util.generateMatrix(current.getStation(), station.getStation());
+                if(!current.equals(station.getFather())){
+                    if((h + depth) < deeper || current.equals(goal)){ //verifica se a distancia do filho ao pai é menor
+                        if(station.getFather() != null && !station.getFather().getLine().contains(current.getLine())){
+                            total = 2;
+                        } else total = 0;
+                        deeper = h;
+                        current.setFather(station);
+                        choice = current;
+                        if(current.equals(goal)) break;
                     }
-                    deeper = depth;
-                    current.setFather(station);
-                    choice = current;
                 }
             }
+            total += (h + depth);
             path.add(choice);
-            search(choice, goal, path, depth);
+            search(choice, goal, path, total);
         }
     }
 
