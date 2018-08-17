9eb73710ed7e424c5351ae9b4618950ecb4decb3
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-08-17 09:35:10 -0300

    Update Node.java

--- Primeira Prova/Q4/src/Node.java
+++ Primeira Prova/Q4/src/Node.java
@@ -18,27 +18,25 @@ class Node {
         Node root = new Node();
         boolean condition = false;
 
+        showBoard(root.board);
         System.out.println("Escolha uma coordenada ' - ' para marcar: (linha, coluna)");
         int line = in.nextInt();
         int column = in.nextInt();
         root.board[line][column] = "X";
+        showBoard(root.board);
         generate(root);
-        /*while(!condition){
-            if(i >= 1) break;
+        minimax(root);
+        root = getChoice(root);
+
+        while(check(root.board) || count(root.board) != 0){
             showBoard(root.board);
             System.out.println("Escolha uma coordenada ' - ' para marcar: (linha, coluna)");
             line = in.nextInt();
             column = in.nextInt();
-            //condition = mark(root, line, column);
             root.board[line][column] = "X";
-            root.board[1][1] = "X";
-            root.board[2][1] = "0";
-            root.board[1][2] = "X";
-            root.board[1][0] = "0";
-            generate(root);
-            i++;
-        }*/
-        showBoard(root.board);
+            root = getNode(root, root.board);
+            root = getChoice(root);
+        }
     }
 
     public static String[][] createBoard(){
@@ -87,7 +85,7 @@ class Node {
     }
 
     public static void generate(Node node){
-        if(check(node.board) || count(node.board) == 0) {
+        if(check(node.board)) {
             node.evaluate = evaluation(node.board);
             return;
         } else {
@@ -103,9 +101,9 @@ class Node {
                             newNode.board[line][column] = "X";
                             newNode.max = 0;
                         }
-                        node.children.add(newNode);
-                        showBoard(newNode.board);
+                        //showBoard(newNode.board);
                         generate(newNode);
+                        node.children.add(newNode);
                     }
                 }
             }
@@ -120,9 +118,31 @@ class Node {
         }
     }
 
-        public static void minimax(Node node){
-
+    public static int minimax(Node node){
+        if(check(node.board)){
+            return evaluation(node.board);
+        } else if(node.max == 0){
+            int alpha = -10;
+            for (Node current : node.children){
+                int x = minimax(current);
+                if(x > alpha){
+                    alpha = x;
+                }
+            }
+            node.evaluate = alpha;
+            return alpha;
+        } else{
+            int beta = 10;
+            for (Node current : node.children){
+                int x = minimax(current);
+                if(x < beta){
+                    beta = x;
+                }
+            }
+            node.evaluate = beta;
+            return beta;
         }
+    }
 
     // Checa se o nó atual é uma solução
     public static boolean check(String[][] board){
@@ -134,6 +154,9 @@ class Node {
                 (board[0][0].equals(board[1][0]) && board[1][0].equals(board[2][0])) && !board[1][0].equals("-") ||
                 (board[0][1].equals(board[1][1]) && board[1][1].equals(board[2][1])) && !board[1][1].equals("-") ||
                 (board[0][2].equals(board[1][2]) && board[1][2].equals(board[2][2])) && !board[1][2].equals("-") ){
+            return true;
+        }
+        if(count(board) == 0){
             return true;
         }
         return false;
@@ -159,19 +182,39 @@ class Node {
             if(board[1][1].equals("X")){
                 return -1;
             } else return 1;
-        }
-        if( (board[0][0].equals(board[1][0]) && board[1][0].equals(board[2][0])) && !board[1][0].equals("-")){
+        } else if( (board[0][0].equals(board[1][0]) && board[1][0].equals(board[2][0])) && !board[1][0].equals("-")){
             if(board[1][1].equals("X")){
                 return -1;
             } else return 1;
-        }
-        if( (board[0][2].equals(board[1][2]) && board[1][2].equals(board[2][2])) && !board[1][2].equals("-")){
+        } else if( (board[0][2].equals(board[1][2]) && board[1][2].equals(board[2][2])) && !board[1][2].equals("-")){
             if(board[1][1].equals("X")){
                 return -1;
             } else return 1;
-        }
-        return 0;
+        } return 0;
     }
+
+    public static Node getChoice(Node node){
+        int max = -10;
+        Node choice = null;
+        for(Node current : node.children){
+            if(current.evaluate > max){
+                max = current.evaluate;
+                choice = current;
+            }
+        }
+        return choice;
+    }
+
+    public static Node getNode(Node root, String[][] board){
+        for(Node current : root.children){
+            if (Arrays.equals(current.board[0],board[0]) && Arrays.equals(current.board[1],board[1])
+                    && Arrays.equals(current.board[2],board[2])){
+                return current;
+            }
+        }
+        return null;
+    }
+
 
     public static void showBoard(String[][] board){
         System.out.printf("    0   1   2\n   ___________\n");
